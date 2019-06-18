dea96d8c2774553a38544d23ed09d649e06190fa
Daniel Elias <danielek7@gmail.com>
2019-06-18 17:10:33 +1000

    name variables changed

--- js/main.js
+++ js/main.js
@@ -2,9 +2,9 @@ document.addEventListener("DOMContentLoaded", () => {
   $.getJSON("../public/race.json", function(result) {
     showlist(result);
 
-    document.querySelectorAll(".gender-filter").forEach(button => {
+    document.querySelectorAll(".sexo-filter").forEach(button => {
       button.onclick = () => {
-        showlist(result.filter(item => item.Sexo === button.dataset.gender));
+        showlist(result.filter(item => item.Sexo === button.dataset.sexo));
         return false;
       };
     });
@@ -14,9 +14,12 @@ document.addEventListener("DOMContentLoaded", () => {
 function showlist(list) {
   let output = "";
   list.forEach(item => {
-    output += `<tr><td>${item.Nombres}</td><td>${item.Apellidos}</td><td>${
-      item.Sexo
-    }</td></tr>`;
+    output += `
+    <tr>
+      <td>${item.Nombres}</td>
+      <td>${item.Apellidos}</td>
+      <td>${item.Sexo}</td>
+    </tr>`;
   });
   document.querySelector("#list").innerHTML = output;
 }
